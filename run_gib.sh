583dd6709f310c1cff7839e4beb4197a03ecc73d
RAraghavarora <raghavarora999@yahoo.in>
2023-01-10 15:30:01 +0530

    igibson scenes with replicacad objects

---
+++ run_gib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --partition=long
+#SBATCH -G 2
+#SBATCH -n 4
+#SBATCH --cpus-per-task=1
+#SBATCH -J igib
+#SBATCH --output=bob.out
+#SBATCH --error=bob.err
+#SBATCH --reservation=rrc
+#SBATCH --mail-type=all
+#SBATCH --mail-user=reepicheep_logs@protonmail.com
+#SBATCH --mem-per-cpu=8GB
+
+module load u18/cuda/11.6 u18/cudnn/8.4.0-cuda-11.6
+
+date
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate habitat
+python try_gib.py
+echo $?
